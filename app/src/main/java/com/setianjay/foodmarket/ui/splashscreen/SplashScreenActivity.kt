d41d76214c61d94d651f85e47ffc7dc73a4e2bb0
setianjay <hari.setiaji2000@gmail.com>
2021-03-27 01:52:50 +0700

    Membuat SplashScreen

---
+++ app/src/main/java/com/setianjay/foodmarket/ui/splashscreen/SplashScreenActivity.kt
@@ -0,0 +1,23 @@
+package com.setianjay.foodmarket.ui.splashscreen
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import com.setianjay.foodmarket.MainActivity
+import com.setianjay.foodmarket.R
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        move()
+    }
+
+    private fun move(){
+        Handler().postDelayed({
+            startActivity(Intent(applicationContext,MainActivity::class.java))
+        }, 3000)
+    }
+}
