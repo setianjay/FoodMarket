f013cd18226ec318168a2dbf0a389d82e4ab60b3
setianjay <hari.setiaji2000@gmail.com>
2021-03-27 21:54:18 +0700

    Mengaktifkan action navigation dari Signup ke Signup Address

--- app/src/main/java/com/setianjay/foodmarket/ui/auth/signup/SignUpFragment.kt
+++ app/src/main/java/com/setianjay/foodmarket/ui/auth/signup/SignUpFragment.kt
@@ -5,13 +5,29 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import com.setianjay.foodmarket.R
+import com.setianjay.foodmarket.databinding.FragmentSignUpBinding
 
 class SignUpFragment : Fragment() {
+    private lateinit var binding: FragmentSignUpBinding
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_up, container, false)
+      binding = FragmentSignUpBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initListner()
+    }
+
+    private fun initListner(){
+        binding.btnContinue.setOnClickListener {
+            Navigation.findNavController(it)
+                .navigate(R.id.action_sign_up_address,null)
+        }
     }
 }
